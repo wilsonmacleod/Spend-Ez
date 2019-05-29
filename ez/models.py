037d2be5cfa0765359acf89a9cb7c0ffd07f06fe
Wilson MacLeod <wilsonmacleod@gmail.com>
2019-05-29 13:41:53 -0700

    progress and reqs

--- ez/models.py
+++ ez/models.py
@@ -2,22 +2,23 @@ from datetime import datetime
 from ez import db
 
 class User(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable = False)
-    email = db.Column(db.String(120), unique=True, nullable = False)
-    image_file = db.Column(db.String(20), nullable = False, default='default.jpg')
     password = db.Column(db.String(60), nullable =  False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    trans = db.relationship('Transactions', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}')"
 
-class Post(db.Model):
+class Transactions(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable = False)
+    amount = db.Column(db.Integer, nullable = False)
+    note = db.Column(db.Text, nullable=True)
+    cat = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.title}', '{self.date_posted}')"
+        return f"User('{self.amount}', '{self.note}', '{self.cat}', '{self.date_posted}')"
--- ez/routes.py
+++ ez/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect
 from ez import app
 from ez.forms import LoginForm, Expend
-from ez.models import User, Post
+from ez.models import User, Transactions
 
 trans = [
     {
