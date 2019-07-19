fc59045171f33eefb4d8abc4ba6238868ffdcebe
Wilson MacLeod <wilsonmacleod@gmail.com>
2019-07-19 00:32:31 -0700

    slider done, moody started, cleaned up html

--- ez/static/spend-ez.js
+++ ez/static/spend-ez.js
@@ -1,38 +1,78 @@
-var rangeFinder = document.getElementById("budget-range");
-var hiddenForm = document.getElementById("budget-form");
-var currentBudge = document.getElementById("budget_qty");
+document.getElementById("revealButton").addEventListener("click", function () { //"Update Budget Button"
+  this.classList.toggle("hidden");
+  document.getElementById('budgeUpdateForm').classList.toggle("hidden");
 
-currentBudge.innerText = rangeFinder.value;
+  var rangeFinder = document.getElementById("budget-range"); //slider
+  var currentBudge = document.getElementById("budget_qty"); //form value
 
-rangeFinder.oninput = function () {
-  currentBudge.innerHTML = this.value;
-  rangeFinder.title = this.value;
-  hiddenForm.value = currentBudge.innerHTML;
-}
+  currentBudge.innerText = rangeFinder.value;
 
-currentBudge.addEventListener("click", function () {
-  var inputField = document.createElement("input");
-  inputField.type = "text";
-  inputField.value = currentBudge.innerText;
-  currentBudge.innerHTML = '';
-  currentBudge.appendChild(inputField);
-  inputField.focus();
-  inputField.onblur = function () {
-    let val = '';
-    for (x = 0; x < inputField.value.length; x++) {
-      if (!isNaN(inputField.value[x])) {
-        val += inputField.value[x];
-      } else {
-        continue;
-      }
-    }
-    currentBudge.removeChild(inputField);
-    currentBudge.innerHTML = val;
-    hiddenForm.value = val;
+  rangeFinder.oninput = function () {
+    currentBudge.innerHTML = this.value;
+    rangeFinder.title = this.value;
+    var hiddenForm = document.getElementById("budget-form"); //update Python form 
+    hiddenForm.value = currentBudge.innerHTML;
   }
+
+  currentBudge.addEventListener("click", function () { //functions triggered by user clicking on form value
+    function toggleHidden() { //function to hide/reveal other form attributes 
+      document.getElementById('slidecontainer').toggleAttribute("hidden");
+      document.getElementById('buSubmit').toggleAttribute("hidden");
+    }
+    toggleHidden();
+    var inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.classList.add("inputField");
+    inputField.value = currentBudge.innerText;
+    currentBudge.innerHTML = '';
+    currentBudge.appendChild(inputField);
+    inputField.focus();
+    inputField.onblur = function () { //when user "submits" form value
+      toggleHidden();
+      let val = '';
+      for (x = 0; x < inputField.value.length; x++) {
+        if (!isNaN(inputField.value[x])) { //remove non ints
+          val += inputField.value[x];
+        } else {
+          continue;
+        }
+      }
+      if (val.length <= 0 || val <= 150) { //remove outliers 
+        val = 150;
+      }
+      currentBudge.removeChild(inputField);
+      currentBudge.innerHTML = val;
+      var hiddenForm = document.getElementById("budget-form");
+      hiddenForm.value = val;
+    }
+  })
+  document.getElementById("budgetIncrease").addEventListener("click", function () { //increase budget scale incrementally
+    var r = Number(rangeFinder.max);
+    r += 100;
+    rangeFinder.max = r;
+  })
+  document.getElementById("buSubmit").addEventListener("click", function () { //flip back to the beginning when we submit
+    document.getElementById("revealButton").click();
+  })
 })
-document.getElementById("budgetIncrease").addEventListener("click", function () {
-  var r = Number(rangeFinder.max);
-  r += 100;
-  rangeFinder.max = r;
-})
+
+//TODO
+// All imgs local, change titles
+/*
+window.onload = feedback
+
+function feedback () {
+  var img = document.getElementById('howIfeel');
+  var bPerc = document.getElementById("bPerc");
+  var pVal = Number(bPerc.getAttribute("value"));
+  if(pVal >= 120.00){
+    img.src = "https://img.icons8.com/ios/50/000000/headstone.png";
+  }else if(pVal >= 90.00){
+    img.src = "https://img.icons8.com/ios/50/000000/sad.png";
+  }else if(pVal >= 75.00){
+    img.src = "https://icons8.com/icon/25542/embarrassed";
+  }else{
+    img.src = "https://img.icons8.com/ios/50/000000/happy.png";
+  }
+}
+*/
