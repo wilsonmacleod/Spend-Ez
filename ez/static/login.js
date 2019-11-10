b3e97a57d48d7047618e0db5ded9e2d85f48e86b
Wilson MacLeod <wilsonmacleod@gmail.com>
2019-11-10 00:03:36 -0800

    rename and folder structure pre-update work

--- ez/static/login.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    document.getElementById("dLogin").addEventListener("click", function () {
-        var logUsername = document.getElementById("usern");
-        logUsername.value += "ez_demo";
-        var logPW = document.getElementById("passw");
-        logPW.value += "demo123";
-        document.getElementById("loginSubmit").click()
-    })
-}
